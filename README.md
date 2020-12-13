26e0500d99f220984131f57c61813a63b3f3c22e
Shankar Ganesan <shankar@Shankars-MacBook-Pro.local>
2020-12-13 09:21:41 +0530

    Applied the bizzare string incrementer

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ## Instructions
 
 Given a bizarre string, increment the string by 1. Refer to the test cases for more info.
+
+## Functions
+
+bizarreStringIncrementer --> To verify the string, filter the number from string and process the increment operation
+
+pad --> Utility function to handle the zero padding in the numbers
+
+## Logic
+
+step 1 --> Verify the input string has number at the end
+step 2 --> If not a number in the end, append 1 to the end of the string and return
+step 3 --> If number in the end, find all the numbers in string, identify the last number
+step 4 --> process the increment on the last number
+step 5 --> pad the leading zero's based on the input string's last number length
+step 6 --> return the string with the updated last number string
--- bizarreStringIncrementer.js
+++ bizarreStringIncrementer.js
@@ -1,1 +1,49 @@
-// Start your implementation here
+/**
+ * @function checks if the string ends by number and process the math on the number
+ * @param {*} bizarreString 
+ * @return Modified String
+ */
+
+function bizarreStringIncrementer(bizarreString) {
+
+    let isNumberAtTheEnd = isNaN(bizarreString.slice(-1)) ? false : true;
+
+    if (!isNumberAtTheEnd) {
+        return bizarreString + '1';
+    } else {
+        let numbersInString = bizarreString.match(/\d+/g);
+        let lastNumber = numbersInString ? numbersInString.pop() : null;
+        let numberLength = lastNumber.length;
+        let stringEndIndex = bizarreString.length - numberLength;
+        lastNumber++;
+        return bizarreString.slice(0, stringEndIndex) + pad(lastNumber, numberLength);
+    }
+}
+
+/**
+ * @function - utility function for padding the leading zeros
+ * @param {*} number - number to be padded with leading 0 as a string
+ * @param {*} length - target length with leading 0
+ * @return - padded string
+ */
+
+function pad(number, length) {
+    let str = '' + number;
+    while (str.length < length) {
+        str = '0' + str;
+    }
+    return str;
+}
+
+
+
+
+console.log('foo -->', bizarreStringIncrementer('foo'));
+console.log('foo23 -->', bizarreStringIncrementer('foo23'));
+console.log('foo0041 -->', bizarreStringIncrementer('foo0041'));
+console.log('foo9 -->', bizarreStringIncrementer('foo9'));
+console.log('foo099 -->', bizarreStringIncrementer('foo099'));
+console.log('f99oo -->', bizarreStringIncrementer('f99oo'));
+console.log('f99oo23 -->', bizarreStringIncrementer('f99oo23'));
+console.log('f99oo099 -->', bizarreStringIncrementer('f99oo099'));
+console.log('f99oo0099 -->', bizarreStringIncrementer('f99oo0099'));
